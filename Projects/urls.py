5a503e76b1a3e27310ea8dc77f42da8fe88a06ed
Jose Velasco <jose.juarez.velasco@gmail.com>
2019-07-21 01:46:17 -0700

    added pagination the projects.html, adjusted responsiveness of site
     added pagination to project view, added comments to explain certian
     lines of code, added filler text to index.html, added pagination html
     to project.html and basic css, solved footer not staying at bottom when
     not enough content filled a page

--- Projects/urls.py
+++ Projects/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path('', views.home, name='site-home'),
     path('meettheofficers/', views.meettheofficers, name='meettheofficers'),
     path("projects/detail/<int:projectId>/", views.projectDetail, name="projectDetail"),
+    #							^^^ this is used for dynamic url which is the id of the project being requested
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
     
--- Projects/views.py
+++ Projects/views.py
@@ -2,25 +2,37 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Project
 from django.conf import settings
+from django.core.paginator import Paginator
 
 def home(request):
 	return render(request, 'projectsHTML/index.html')
+	#						^^^^ this will be rendered when it is requested by the url
 
 def meettheofficers(request):
 	return render(request, 'projectsHTML/index.html')
 
 def projectDetail(request, projectId):
+	# this will get a specific project based on it id which will be used to be render details in projectdetail.html
 	instance = get_object_or_404(Project, id=projectId)
+	# context are like variables that can be rendered in the html using double curly braces with its key inbetween
 	context = {
 		"instance": instance,
 	}
 	return render(request, "projectsHTML/projectDetail.html", context)
 
 def projects(request):
-	queryset = Project.objects.all()
+	# this will get all obbjects in the project which can be for looped in the corresponding html
+	queryset_list = Project.objects.all()
+	paginator = Paginator(queryset_list, 9)
+	#									^^^ this is the number that will determine amount of projects per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	queryset = paginator.get_page(page)
 	context = {
 		"Project_list": queryset,
-		"MEDIA_URL": settings.MEDIA_URL
+		"MEDIA_URL": settings.MEDIA_URL,
+		"page_request_var": page_request_var,
+
 	}
 	return render(request, 'projectsHTML/projects.html', context)
 
